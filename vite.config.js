7c5b9e1f591407eafb8e80773a1b96be1c623994
concept-forge <concept_forge@protonmail.com>
2026-04-18 18:00:26 +0200

    Remove vite-plugin-pwa, use manual service worker

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['images/**/*'],
-      manifest: {
-        name: 'Maesters Codex',
-        short_name: 'Maesters',
-        description: 'A fan-made compendium of the Known World',
-        theme_color: '#080808',
-        background_color: '#080808',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: '/images/Logo.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable'
-          },
-          {
-            src: '/images/Logo.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,jpg,svg,json}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365
-              }
-            }
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()],
 })
